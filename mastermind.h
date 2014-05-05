3c178837b21079c92795c238123acb5e00cd65f3
philipwh <philipwh@vt.edu>
2014-05-05 17:52:52 -0400

    Create mastermind.h

---
+++ mastermind.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <stdio.h>
+
+using std::istream;
+using std::ostream;
+using std::string;
+
+using std::endl;
+void mastermind(istream&in, ostream&out);
