26e2c2187a6c72dced0e92a1c413bfdf261b4a35
philipwh <philipwh@vt.edu>
2014-05-12 12:22:11 -0400

    Added header and method comments to Mastermind.cpp

--- Mastermind.cpp
+++ Mastermind.cpp
@@ -1,10 +1,22 @@
+// Mastermind
+// Kim Sibol, Christian Wendt, Philip Whitcomb
+// ECE 2524: Unix
+// 
+// Mastermind is a game of intelligence, where the player must guess
+// the correct number sequence to win the game. Black and white pin
+// variables correspond to correct guesses and number placements.
+//
+
 #include "mastermind.h"
 
+// Generates a random string of numbers used as the random numbers
+// in the game. Takes length as a parameter and returns the char array
+// of numbers.
 char *randomStrGen(int length)
 {
 	static string charset="0123456";
 	char *charArray;
-	charArray = new char[length];
+	charArray = new char[length]; //allocation for char array length
 	srand(time(NULL));
 	for(int i=0; i < 5; i++)
 		charArray[i]=charset[rand() % charset.length()];
@@ -12,14 +24,18 @@ char *randomStrGen(int length)
 	return charArray;
 }
 
-
+// Main in-game method for Mastermind. 
 void mastermind( istream&in, ostream&out)
 {
-	char *number = randomStrGen(5);
+	
+	char *number = randomStrGen(5); //call to randomStrGen method with default length 5
 	//for(int i=0; i<5; i++)
 	//{
 //		out << number[i];
 	//}
+
+	//Instructions displayed at the start of the game
+	//User must type "begin" to start the game
 	out << "Instructions:" << endl;
 	out << "Welcome to Mastermind! You are a code breaker whose objective is to crack the secret code ";
 	out << "created by Dr. Random. The code is comprised of 5 digits where each digit is between 0-6.";
@@ -27,6 +43,7 @@ void mastermind( istream&in, ostream&out)
 	out << "the correct code. Good luck, and let the code breaking begin!" << endl;
 	out << "\n\nPlease type begin to begin the game!\n";
 
+	//Initialization and declaration of variables
 	string begin;
 	string guess;
 	int i=0;
@@ -37,13 +54,16 @@ void mastermind( istream&in, ostream&out)
 	int guesses=0;
 	char *ARRAY;
 	ARRAY = new char[5];
-	in >> begin;
+	in >> begin; 
+
+	//Checks if user enters begin to start the game
 	if(begin=="begin")
 	{
 		out << "Please enter your first guess: ";
 		in >> guess;
 		strcpy(ARRAY, guess.c_str());
 
+		//Loop while guesses are still available
 		while(!in.fail() && guesses < 9)
 		{
 
@@ -89,7 +109,7 @@ void mastermind( istream&in, ostream&out)
 			}
 			strcpy(ARRAY, guess.c_str());
 			guesses++;
-			
+
 			if(blackPin==5)
 			{
 				out << "\nGame over!!!"<< endl;
